498a3aabe7c7fe90963fe73010c4d37dd31ec717
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-10-09 00:27:05 +0530

    Update code

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+from fastapi.staticfiles import StaticFiles  # Import StaticFiles
 
 # Create an instance of the FastAPI class
 app = FastAPI()
 
 # Set up the template directory for Jinja2
 templates = Jinja2Templates(directory="templates")
+
+# Mount the static files directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Define the route for the root URL ("/")
 @app.get("/", response_class=HTMLResponse)
